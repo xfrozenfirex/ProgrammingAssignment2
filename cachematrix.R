71cc2fccc2a0b57bfe6116cdc3b1f195a598f004
xfrozenfirex <xfrozenfirex@gmail.com>
2014-11-21 10:46:28 -0800

    Implemented makeCacheMatrix() and cacheSolve()
    Per programming assignment #2, I've implemented makeCacheMatrix() and cacheSolve().

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The below functions provide a way to optimize matrix inversion calculations
+## by caching the previously calculated inverse matrix. If the values in the
+## original matrix are changed, then the cache is cleared to indicate that a new
+## inverse matrix needs to be recalculated.
 
-## Write a short comment describing this function
+## makeCacheMatrix() creates the object that represents the original matrix and
+## its previously calculated inverse matrix (or NULL if the inverse matrix for
+## the current data has not yet been calculated.) It has 4 functions:
+##    * set(inputtedMatrix)
+##    * get()
+##    * setInverse(inputtedInverseMatrix)
+##    * getInverse()
 
 makeCacheMatrix <- function(x = matrix()) {
-
+   cachedInverse <- NULL
+   set <- function(inputtedMatrix) {
+      x <<- inputtedMatrix
+      cachedInverse <<- NULL
+   }
+   get <- function() x
+   setInverse <- function(inputtedInverse) cachedInverse <<- inputtedInverse
+   getInverse <- function() cachedInverse
+   list(set = set,
+          get = get,
+          setInverse = setInverse,
+          getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve() utilizes makeCacheMatrix() to use the cached inversed matrix.
+## If there is no cached inversed matrix, then it is calculated and then
+## saved in the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+   inversedMatrix <- x$getInverse()
+   if(!is.null(inversedMatrix)) {
+      message("Getting cached data")
+   }
+
+   else {
+      dataMatrix <- x$get()
+      inversedMatrix <- solve(dataMatrix)
+      x$setInverse(inversedMatrix)
+   }
+   return(inversedMatrix)
 }
